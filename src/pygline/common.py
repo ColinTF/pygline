565b4d75f0137766257817eed6a5f76cea4ba10c
ColinTF <colin.t.fin@gmail.com>
2022-03-25 13:40:33 -0700

    added pdocs

--- src/pygline/common.py
+++ src/pygline/common.py
@@ -15,35 +15,35 @@ class Vertex:
     The data is stored as x,y,z,r,g,b attributes. However the data can be accessed and modified in a variety of ways.
 
     Attributes:
-        - x,y,z : np.float32 - 3d coordinates of vertex
-        - r,g,b : np.float32 - color of vertex
+        - `x`,`y`,`z` : `np.float32` - 3d coordinates of vertex
+        - `r`,`g`,`b` : `np.float32` - color of vertex
 
     Methods:
-        - copy() -> Vertex - returns a copy of the vertex
-        - magnitude() -> np.float32 - returns the magnitude of the vertex
-        - brightness() -> np.float32 - returns the brightness of the vertex
-        - normalize() -> None - normalizes the vertex to a unit vector
+        - `copy()` -> `Vertex` - returns a copy of the vertex
+        - `get_magnitude()` -> `np.float32` - returns the magnitude of the vertex
+        - `get_brightness()` -> `np.float32` - returns the brightness of the vertex
+        - `normalize()` -> `None` - normalizes the vertex to a unit vector
 
     ---
 
-    ### Usage
+    ## Usage
 
     Groups of vertices are used to define objects through the Mesh Class and what color they should be.
     Use the `vert.xyz` or `vert.rgb` or variants like `vert.zxy` and `vert.xyrg` to use the data as a numpy array in the order of the attributes.
     Use the `vert.coords` or `vert.color` to get the just the positional or color data as a numpy array. The functionality is designed to be very flexible however,
-    the user should be familiar with how the class works.
+    the user should be familiar with how the class works. Comparison and math operations only affect the positional data and behave similarly to assignment
 
-    Comparison and math operations only affect the positional data and behave similarly to assignment:
-        - `==` : compares the x,y,z values element wise
-        - `<`,`>` : compares the magnitude of the vertex
+    Operations:
+        - `==` : compares the `x`,`y`,`z` values element wise
+        - `<`,`>` : compares the `magnitude()` of the vertex
         - `+`,`-`,`*`,`/` : acts accordingly based on the type of the other object
             - if other is a Vertex : acts element wise
             - if other is a array : acts element wise for values that exist in array
-            - if other is a number : acts on all x,y,z values
+            - if other is a number : acts on all `x`,`y`,`z` values
 
     ---
 
-    ### Notes
+    ## Notes
 
     All values are stored a numpy float32 which hold 32 bits or 4 bytes of data. This is helpful to know the exact size for createing vertex buffers.
     """
@@ -111,11 +111,11 @@ class Vertex:
         """Retuns a copy of the vertex"""
         return Vertex(self.x, self.y, self.z, self.r, self.g, self.b)
 
-    def magnitude(self):
+    def get_magnitude(self):
         """Returns the magnitude of the vertex"""
         return np.sqrt(self.x**2 + self.y**2 + self.z**2)
 
-    def brightness(self):
+    def get_brightness(self):
         """Returns the brightness of the vertex"""
         return np.sqrt(self.r**2 + self.g**2 + self.b**2)
 
