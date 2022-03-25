5ab31745aa5c000b3caf1f60bb08a13dd56ca395
ColinTF <colin.t.fin@gmail.com>
2022-03-25 03:26:00 -0700

    Added more vertex tests

--- src/tests/test_classes.py
+++ src/tests/test_classes.py
@@ -16,9 +16,9 @@ from pygline.common import Vertex
 from tests import entity as ent
 
 
-class Test_Classes:
+class Test_Vertex:
 
-    def test_vertex_one(self):
+    def test_one(self):
 
         print("\n\n")
 
@@ -32,27 +32,32 @@ class Test_Classes:
         vert1.z = 0.07
         vert2.x = 0.35
 
-        vert3 = Vertex(0.2, 0.2, 0.2, b=0)
+        vert3 = Vertex([0.2, 0.2, 0.2])
+
+        vert4 = Vertex(1)
+        print(vert4)
 
         print(vert1)
         print(vert2)
         print(vert3)
         print(vert3.bxy)
+        vert4 = Vertex(1, 2, 3)
+        print(vert4)
+        print(vert4.xyz)
+        vert4.xyz *= 1.2
+        print(vert4.__dict__)
         # assert Vertex(0.2, 0.2) > Vertex(0, 0.5) == False
 
-    def test_vertex_two(self):
+    def test_two(self):
 
-        print("\n\n")
-
-        vert1 = Vertex(7, 12, 3)
-        vert1.xr = 0.5, 0.02
-
-        print(vert1)
-        print(vert1.xyr)
-        print(vert1.coords)
-        vert1.coords = 0.03, 0.97
-        print(vert1.coords)
-        vert1.coords = 0.5
-        print(vert1.coords)
-        vert1.rg = 0.1 , 0.3
-        print(vert1.color)
+        assert Vertex(1) > 0.5
+        assert Vertex(1) > Vertex(0.5)
+        assert Vertex(1, 5, 3) > Vertex(3, 2, 1, b=2.0)
+        assert Vertex(2, 4, 5) - Vertex(1, 3, 5) == Vertex(1, 1, 0)
+        assert Vertex(1, 2, 3) + Vertex(1, 2, 3) == Vertex(2, 4, 6)
+        assert Vertex(5, 2, 8) * Vertex(1, 2, 3) == Vertex(5, 4, 24)
+        assert Vertex(12, 16, 24) * 0.5 == Vertex(6, 8, 12)
+        assert Vertex(12, 16, 24) / 2 == Vertex(6, 8, 12)
+        assert Vertex(2, 4, 6) + [1, 2] == Vertex(3, 6, 6)
+        assert Vertex(2, 4, 6) * [1, 2] == Vertex(2, 8, 6)
+        assert Vertex(5, 2, 8) / Vertex(1, 2, 3) == Vertex(5, 1, 8/3)
